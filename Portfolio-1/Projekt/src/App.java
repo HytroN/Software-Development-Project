d5bc7f4cfa400985def3bd90816051018e025549
Charlie Nielsen <54167446+HytroN@users.noreply.github.com>
2022-09-15 09:33:56 +0200

    Testing changes

--- Portfolio-1/Projekt/src/App.java
+++ Portfolio-1/Projekt/src/App.java
@@ -1,12 +1,13 @@
 import org.junit.Test;
 
 public class App {
+
     public static void main(String[] args) throws Exception {
-        print("Hello World");
+        System.out.println("Hello World " + sum(4, 5));
     }
 
     @Test
-    public static void print(String text) {
-        System.out.println(text);
+    public static int sum(int x, int y) {
+        return x + y;
     }
 }
