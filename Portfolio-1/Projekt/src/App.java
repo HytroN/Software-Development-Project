fffb628b3a2ded57e3af81c78dae1aa211281d68
Charlie Nielsen <54167446+HytroN@users.noreply.github.com>
2022-09-15 08:44:37 +0200

    updated .gitignore

--- Portfolio-1/Projekt/src/App.java
+++ Portfolio-1/Projekt/src/App.java
@@ -1,7 +1,12 @@
+import org.junit.Test;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("hej med dig");
+        print("Hello World");
     }
 
+    @Test
+    public static void print(String text) {
+        System.out.println(text);
+    }
 }
