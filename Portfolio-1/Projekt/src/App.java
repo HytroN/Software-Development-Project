d2cd21cce2ef3045a219684aa1a19fa87724be91
Charlie Nielsen <54167446+HytroN@users.noreply.github.com>
2022-09-25 15:11:37 +0200

    Update App.java

--- Portfolio-1/Projekt/src/App.java
+++ Portfolio-1/Projekt/src/App.java
@@ -1,13 +1,71 @@
-import org.junit.Test;
+import java.util.Vector;
 
 public class App {
-
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello World " + sum(4, 5));
+        System.out.println("Hello, World!");
+        Vessels k = new Vessels();
+
+        k.Vessel("Denmark", "Roro", 10, 200, 10);
+    }
+}
+
+class Vessels {
+    String vesselType, flagNation;
+    int draft, length, width;
+
+    public void Vessel(String flagNation, String vesselType, int draft, int length, int width) {
+        this.flagNation = flagNation;
+        this.vesselType = vesselType;
+        this.draft = draft;
+        this.length = length;
+        this.width = width;
+
+        System.out.println("Flagnation: " + flagNation + " | Vessel Type: " + vesselType + " | draft: " + draft
+                + " | length: " + length + " | width: " + width);
+    }
+}
+
+class RoRo extends Vessels {
+    int carsLength;
+    int truckLength;
+
+    void loadingCargo() {
+
     }
 
-    @Test
-    public static int sum(int x, int y) {
-        return x + y;
+    void utilityLevelOfCapacity() {
+
+    }
+}
+
+class Tankers extends Vessels {
+    int compartments;
+
+    public Tankers(String flagNation, int draft, int length, int width) {
+
+    }
+
+    void loadingCargo() {
+
+    }
+
+    void utilityLevelOfCapacity() {
+
+    }
+}
+
+class Containers extends Vessels {
+    int amountOfContainers;
+
+    public Containers(String flagNation, int draft, int length, int width) {
+
+    }
+
+    void loadingCargo() {
+
+    }
+
+    void utilityLevelOfCapacity() {
+
     }
 }
